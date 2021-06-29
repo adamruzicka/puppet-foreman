2f702b2f405f44bb75eb46962ce4235656edb62e
Adam Ruzicka <aruzicka@redhat.com>
2021-06-29 10:26:33 +0200

    Add foreman_host type and provider

---
+++ lib/puppet/provider/foreman_host/rest_v3.rb
@@ -0,0 +1,55 @@
+Puppet::Type.type(:foreman_host).provide(:rest_v3, :parent => Puppet::Type.type(:foreman_resource).provider(:rest_v3)) do
+  confine :feature => [:json, :oauth]
+
+  def exists?
+    !id.nil?
+  end
+
+  def create
+    path = "api/v2/hosts/facts"
+    payload = {
+      :name => resource[:name],
+      :certname => resource[:name],
+      :facts => resource[:facts]
+    }.to_json
+    r = request(:post, path, {}, payload)
+
+    unless success?(r)
+      error_string = "Error making PUT request to Foreman at #{request_uri(path)}: #{error_message(r)}"
+      raise Puppet::Error.new(error_string)
+    end
+  end
+
+  def destroy
+    r = request(:delete, destroy_path, {})
+
+    unless success?(r)
+      error_string = "Error making DELETE request to Foreman at #{request_uri(path)}: #{error_message(r)}"
+      raise Puppet::Error.new(error_string)
+    end
+  end
+
+  def id
+    host['id'] if host
+  end
+
+  def host
+    @host ||= begin
+      path = 'api/v2/hosts'
+      r = request(:get, path, :search => %{name="#{resource[:name]}"})
+
+      unless success?(r)
+        error_string = "Error making GET request to Foreman at #{request_uri(path)}: #{error_message(r)}"
+        raise Puppet::Error.new(error_string)
+      end
+
+      JSON.load(r.body)['results'].first
+    end
+  end
+
+  private
+
+  def destroy_path
+    "api/v2/hosts/#{id}"
+  end
+end
---
+++ lib/puppet/type/foreman_host.rb
@@ -0,0 +1,49 @@
+Puppet::Type.newtype(:foreman_host) do
+  desc 'foreman_host creates a host in foreman.'
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'The name of the host.'
+  end
+
+  newparam(:facts) do
+    defaultto {}
+  end
+
+  newparam(:base_url) do
+    desc 'Foreman\'s base url.'
+  end
+
+  newparam(:effective_user) do
+    desc 'Foreman\'s effective user for the registration (usually admin).'
+  end
+
+  newparam(:consumer_key) do
+    desc 'Foreman oauth consumer_key'
+  end
+
+  newparam(:consumer_secret) do
+    desc 'Foreman oauth consumer_secret'
+  end
+
+  newparam(:ssl_ca) do
+    desc 'Foreman SSL CA (certificate authority) for verification'
+  end
+
+  newparam(:timeout) do
+    desc "Timeout for HTTP(s) requests"
+
+    munge do |value|
+      value = value.shift if value.is_a?(Array)
+      begin
+        value = Integer(value)
+      rescue ArgumentError
+        raise ArgumentError, "The timeout must be a number.", $!.backtrace
+      end
+      [value, 0].max
+    end
+
+    defaultto 500
+  end
+end
