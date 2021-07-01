bc4699e223c00de1a6ecc8bf78e585de825daad1
Adam Ruzicka <aruzicka@redhat.com>
2021-07-01 13:21:08 +0200

    Pass facts to upload

--- manifests/register.pp
+++ manifests/register.pp
@@ -10,6 +10,7 @@ class foreman::register (
     consumer_secret => $foreman::oauth_consumer_secret,
     effective_user  => $foreman::oauth_effective_user,
     ssl_ca          => $foreman::server_ssl_ca,
+    facts           => $facts,
   }
 
   foreman_instance_host { $foreman_host_name:
